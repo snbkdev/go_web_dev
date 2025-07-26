5c9abfa849bea53aa53fdba569c580694a996ea0
Asanbek09 <samudinua@gmail.com>
2025-07-26 18:36:21 +0600

    homeHandler изменения

--- web_project/cmd/web/main.go
+++ web_project/cmd/web/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
-	"text/template"
 )
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles("templates/hello.gohtml")
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
+	}
+}
 
 type User struct {
 	Name string
