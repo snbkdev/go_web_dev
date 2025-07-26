ae2a36471ff05e2819496fcd7dba6491e2bcb4dc
Asanbek09 <samudinua@gmail.com>
2025-07-26 10:33:21 +0600

    FAQ страница

--- web_project/main.go
+++ web_project/main.go
@@ -15,31 +15,42 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:test@test.dev\">test@test.dev</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li>Is there a free version ? Yes! We offer a free trial for 30 days on any paid plans</li>
+	</ul>`)
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
-//type Router struct{}
-
-//func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//	switch r.URL.Path {
-//	case "/":
-//		homeHandler(w, r)
-//	case "/contact":
-//		contactHandler(w, r)
-//	default:
-//		http.Error(w, "Page not found", http.StatusNotFound)
-//	}
-//}
-
 func main() {
+	var router Router
 	fmt.Println("Starting the server on :3500...")
-	http.ListenAndServe(":3500", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3500", router)
 }
