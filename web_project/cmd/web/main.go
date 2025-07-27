810e9d7cd5904c74295cd4bab0ccf993fe29b896
Asanbek09 <samudinua@gmail.com>
2025-07-27 09:38:14 +0600

    FAQ страница изменена

--- web_project/cmd/web/main.go
+++ web_project/cmd/web/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "index.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
