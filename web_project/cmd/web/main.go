0252e8c10a2fe59ff801274788961cee6d0a8aad
Asanbek09 <samudinua@gmail.com>
2025-08-10 17:23:05 +0600

    Реализуем страницу Sign In

--- web_project/cmd/web/main.go
+++ web_project/cmd/web/main.go
@@ -37,7 +37,10 @@ func main() {
 
 	usersC := controllers.Users{UserService: &userService, }
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
+
 	r.Get("/signup", usersC.New)
+	r.Get("/signin", usersC.SignIn)
 	r.Post("/users", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
