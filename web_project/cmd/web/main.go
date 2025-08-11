d4165db2275f1618d23c89c5db4eaca0bd7efed3
Asanbek09 <samudinua@gmail.com>
2025-08-11 17:16:33 +0600

    Работа над cookie

--- web_project/cmd/web/main.go
+++ web_project/cmd/web/main.go
@@ -43,6 +43,7 @@ func main() {
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/users", usersC.Create)
 	r.Post("/signin", usersC.ProcessSignIn)
+	r.Get("/users/me", usersC.CurrentUser)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
