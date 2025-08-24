a6fdd5fa8e4b93be18a03519fdb3db708d572d33
Asanbek09 <samudinua@gmail.com>
2025-08-24 14:01:40 +0600

    Работа над session.go -> token

--- web_project/models/session.go
+++ web_project/models/session.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 	"web_project/rand"
 )
@@ -33,11 +35,10 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
-
 	session := Session{
 		UserID: userID,
 		Token: token,
-
+		TokenHash: ss.hash(token),
 	}
 
 	return &session, nil
@@ -45,4 +46,9 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 
 func (ss *SessionService) User(token string) (*User, error) {
 	return nil, nil
+}
+
+func (ss *SessionService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
