6592b5cf513620c2d3bf6abd8f3fe012d14226f9
Asanbek09 <samudinua@gmail.com>
2025-09-17 15:27:42 +0600

    Работа над sign in/out в шаблоне

--- web_project/views/template.go
+++ web_project/views/template.go
@@ -8,6 +8,8 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"web_project/models"
+	"web_project/context"
 
 	"github.com/gorilla/csrf"
 )
@@ -25,6 +27,9 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 		template.FuncMap{
 			"csrfField": func() (template.HTML, error) {
 				return "", fmt.Errorf("csrfField not implemented")
+			},
+			"currentUser": func() (template.HTML, error) {
+				return "", fmt.Errorf("currentUser not implemented")
 			},
 		},
 	)
@@ -63,6 +68,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 	tpl = tpl.Funcs(template.FuncMap{
 			"csrfField": func() template.HTML {
 				return csrf.TemplateField(r)
+			},
+			"currentUser": func() *models.User {
+				return context.User(r.Context())
 			},
 		},
 	)
