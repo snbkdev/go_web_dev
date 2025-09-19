c06b8514e40a33556b34df0b8d60e2d8b61ab6d5
Asanbek09 <samudinua@gmail.com>
2025-09-19 15:34:40 +0600

    Отправка писем

--- web_project/models/email.go
+++ web_project/models/email.go
@@ -1,21 +1,33 @@
 package models
 
-import "github.com/go-mail/mail/v2"
+import (
+	"fmt"
+
+	"github.com/go-mail/mail/v2"
+)
 
 const (
 	DefaultSender = "***" // Нужна реальная почта
 )
 
+type Email struct {
+	From      string
+	To        string
+	Subject   string
+	Plaintext string
+	HTML      string
+}
+
 type SMTPConfig struct {
-	Host string
-	Port int
+	Host     string
+	Port     int
 	Username string
 	Password string
 }
 
 func NewEmailService(config SMTPConfig) (*EmailService, error) {
 	es := EmailService{
-		dialer : mail.NewDialer(config.Host, config.Port, config.Username, config.Password),
+		dialer: mail.NewDialer(config.Host, config.Port, config.Username, config.Password),
 	}
 	return &es, nil
 }
@@ -24,4 +36,40 @@ type EmailService struct {
 	DefaultSender string
 
 	dialer *mail.Dialer
+}
+
+func (es *EmailService) Send(email Email) error {
+	msg := mail.NewMessage()
+	msg.SetHeader("To", email.To)
+
+	es.setFrom(msg, email)
+	msg.SetHeader("Subject", email.Subject)
+	switch {
+		case email.Plaintext != "" && email.HTML != "":
+			msg.SetBody("text/plain", email.Plaintext)
+			msg.AddAlternative("text/html", email.HTML)
+		case email.Plaintext != "":
+			msg.SetBody("text/plain", email.Plaintext)
+		case email.HTML != "":
+			msg.SetBody("text/html", email.HTML)
+	}
+
+	err := es.dialer.DialAndSend(msg)
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+}
+
+func (es *EmailService) setFrom(msg *mail.Message, email Email) {
+	var from string
+	switch {
+	case email.From != "":
+		from = email.From
+	case es.DefaultSender != "":
+		from = es.DefaultSender
+	default:
+		from = DefaultSender
+	}
+	msg.SetHeader("From", from)
 }
